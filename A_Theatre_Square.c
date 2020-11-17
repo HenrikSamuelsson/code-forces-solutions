68c3cf7e9d1a05b4088eed9f8d1ef12341f1d9a5
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-11-17 18:53:25 +0100

    Start using the ceil function.

--- A_Theatre_Square.c
+++ A_Theatre_Square.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,29 +7,17 @@ int main(void)
     long long n;
     long long m;
     long long a;
-    long long c;
-    long long r;
+    long long columns;
+    long long rows;
+    long long area;
 
     scanf("%lld %lld %lld", &n, &m, &a);
-    if (0 == n % a)
-    {
-        c = n / a;
-    }
-    else
-    {
-        c = n / a + 1;
-    }
 
-    if (0 == m % a)
-    {
-        r = m / a;
-    }
-    else
-    {
-        r = m / a + 1;
-    }
+    columns = (long long)ceil((double)n / (double)a);
+    rows = (long long)ceil((double)m / (double)a);
+    area = columns * rows;
 
-    printf("%lld\n", r * c);
+    printf("%lld\n", area);
 
     return EXIT_SUCCESS;
 }
