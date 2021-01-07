7170e7472d00faa162e687cd3fa43c861be96632
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2021-01-07 22:14:46 +0100

    Add start of a beatiful matrix.

---
+++ c-plus-plus/p-263a-beautiful-matrix/beautiful-matrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    const int lineCount = 5;    // Number of lines in the matrix.
+    const int colCount = 5;    // Number of columns in the matrix.
+
+    int lineIndex = 0;
+    int colIndex = 0;
+
+    std::string line;
+    for (int l = 0; l < lineCount; l++)
+    {
+        std::getline(std::cin, line);
+        for (int c = 0; c < colCount; c++)
+        {
+            
+        }
+    }
+
+    return 0;
+}
