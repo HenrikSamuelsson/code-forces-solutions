b9509f84c08db19aa4c152af3ebe504917649585
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2021-01-16 13:19:32 +0100

    Version that passed the tests.

--- c-plus-plus/p-1407a-ahahahahahahahaha/ahah.cpp
+++ c-plus-plus/p-1407a-ahahahahahahahaha/ahah.cpp
@@ -7,7 +7,8 @@
 
 template<class T>
 void write_vector(const std::vector<T>& vector) {
-    std::cout << "Numbers you entered: ";
+    //std::cout << "Numbers you entered: ";
+    std::cout << vector.size() << std::endl;
     std::copy(vector.begin(), vector.end(),
         std::ostream_iterator<T>(std::cout, " "));
     std::cout << '\n';
@@ -29,15 +30,33 @@ int main ()
         std::getline(std::cin, line);
         
         std::vector<int> numbers;
+        std::vector<int> results;
         std::getline(std::cin, line);
         std::istringstream stream(line);
+        
         int number;
         while (stream >> number)
         {
            numbers.push_back(number);
         }
-
-        write_vector(numbers);
+        
+        // Traverse backwards
+        for (int i = numbers.size() - 1; i >= 1; i -= 2)
+        {
+            if ((numbers[i] == 1 && numbers[i - 1] == 0) || 
+                (numbers[i] == 0 && numbers[i - 1] == 1))
+            {
+                results.insert(results.begin(), 0);
+            }
+            else
+            {
+                results.insert(results.begin(), numbers[i]);
+                results.insert(results.begin(), numbers[i - 1]);
+            }
+        }
+        
+        write_vector(results);
+        
     }
 
     return 0;
