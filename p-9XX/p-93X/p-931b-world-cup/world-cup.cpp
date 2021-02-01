de43a1f1563f977edf18cea3650ee71164b06a5e
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-02-01 19:41:09 +0100

    Add start of solution.

---
+++ p-9XX/p-93X/p-931b-world-cup/world-cup.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int n, a, b;
+    cin >> n >> a >> b;
+
+    int r = -1;
+    while (n > 0)
+    {
+        n /= 2;
+        r++;
+    }
+
+    cout << r;
+
+    return 0;
+}
