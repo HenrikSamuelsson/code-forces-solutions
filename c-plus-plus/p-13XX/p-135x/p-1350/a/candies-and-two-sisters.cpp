c52b00827724c19ff4c225edd726f50ac776a78f
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2021-02-24 14:55:19 +0100

    Add start of CPP solution for problem 1335 A

---
+++ c-plus-plus/p-13XX/p-135x/p-1350/a/candies-and-two-sisters.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int t;
+    cin >> t;
+
+    for (size_t i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+
+        cout << n;  // debug test
+    }
+
+    return 0;
+}
