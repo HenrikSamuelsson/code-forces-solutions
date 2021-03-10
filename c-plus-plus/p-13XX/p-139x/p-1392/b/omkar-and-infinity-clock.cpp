effc891a3801c9277c01b21f507247d97fa2c338
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2021-03-10 14:51:20 +0100

    Add start of problem 1392 B.

---
+++ c-plus-plus/p-13XX/p-139x/p-1392/b/omkar-and-infinity-clock.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int t;
+    cin >> t;
+
+    for (size_t i = 0; i < t; i++)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        // TODO read in input into an vector or something similar
+    }
+    
+    return 0;
+}
