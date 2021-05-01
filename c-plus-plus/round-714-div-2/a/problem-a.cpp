f3a9e44d89f58c6b9b6e40286fdb582cd3fc7e63
Henrik Samuelsson <henrik.samuelsson@ka-group.com>
2021-05-01 09:22:34 +0200

    Add cpp solution to problem.

--- c-plus-plus/round-714-div-2/a/problem-a.cpp
+++ c-plus-plus/round-714-div-2/a/problem-a.cpp
@@ -7,5 +7,51 @@ int main()
     int t;
     cin >> t;
 
+    for (size_t i = 1; i <= t; i++)
+    {
+        int n;
+        int k;
+        cin >> n >> k;
+        
+        if (n % 2 == 0)
+        {
+            if (k >= n / 2)
+            {
+                cout << "-1" << endl;
+                continue;
+            }
+        }
+        else
+        {
+            if (k > n / 2)
+            {
+                cout << "-1" << endl;
+                continue;
+            }
+        }
+
+        int threshold = n - k;
+        int max = n;
+        bool first = true;
+        for (size_t j = 1; j <= threshold; j++)
+        {
+            if (first)
+            {
+                cout << j;
+                first = false;
+            }
+            else
+            {
+                cout << " " << j;
+            }
+            if (k-- > 0)
+            {
+                cout << " " << max--;
+            }
+        }
+        cout << endl;
+    
+    }
+
     return 0;
 }
