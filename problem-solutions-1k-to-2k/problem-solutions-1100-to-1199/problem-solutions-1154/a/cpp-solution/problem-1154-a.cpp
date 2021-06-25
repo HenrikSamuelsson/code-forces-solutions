ce88ac403238f00101bbe05687074fcca887bb1b
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-06-25 15:40:20 +0200

    Add CPP solution to problem 1154 A.

---
+++ problem-solutions-1k-to-2k/problem-solutions-1100-to-1199/problem-solutions-1154/a/cpp-solution/problem-1154-a.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>       
+#include <vector>
+
+int main(void)
+{
+    // Vector for the values provided as input.
+    std::vector<int> x;
+    
+    // Read in input from the user, should be four positive integers.
+    const int INPUT_SIZE = 4;
+    
+    int input = 0;
+
+    for (size_t i = 0; i < INPUT_SIZE; i++)
+    {
+        std::cin >> input;
+        x.push_back(input);
+    }
+
+    // Sort the input from the user from lowest highest.
+    std::sort(x.begin(), x.end());
+
+    // Calculate the values to be deduced.
+    int a = x[3] - x[0];    // a = (a + b + c) - (b + c)
+    int b = x[3] - x[1];    // b = (a + b + c) - (a + c)
+    int c = x[3] - x[2];    // c = (a + b + c) - (a + b)
+
+    // Present the result.
+    std::cout << a << " " << b << " " << c << std::endl;
+
+    return EXIT_SUCCESS;
+}
