cf0f4a453b41d3e9419dfd663ee9aba6bb1b5580
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-06-28 17:26:14 +0200

    Add CPP solution for problem 490 a.

---
+++ problem-solutions-0k-to-1k/solutions-400-to-499/solutions-490-to-499/solutions-490/a/cpp/solution-490-a.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+
+int main()
+{
+    int childCount;
+    std::cin >> childCount;
+
+    std::vector<int> programmingIndexes;
+    std::vector<int> mathIndexes;
+    std::vector<int> peIndexes;
+
+    for (size_t c = 1; c <= childCount; c++)
+    {
+        int t;
+        std::cin >> t;
+
+        if (1 == t)
+        {
+            programmingIndexes.push_back(c);
+        }
+        else if (2 == t)
+        {
+            mathIndexes.push_back(c);
+        }
+        else if (3 == t)
+        {
+            peIndexes.push_back(c);
+        }
+    }
+    
+    int min = programmingIndexes.size();
+    if (min > mathIndexes.size())
+    {
+        min = mathIndexes.size();
+    }
+    if (min > peIndexes.size())
+    {
+        min = peIndexes.size();
+    }
+    
+    if (0 == min)
+    {
+        std::cout << 0 << std::endl;
+    }
+    else
+    {
+        std::cout << min << std::endl;
+        for (size_t i = 0; i < min; i++)
+        {
+            std::cout << programmingIndexes[i] << " " << mathIndexes[i] << " " << peIndexes[i] << std::endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
