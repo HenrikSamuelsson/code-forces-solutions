1b4bab04da980bc41c8eb605c816da7190a23d41
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-06-30 10:31:41 +0200

    Add CPP solution for problem 1399 B.

--- problem-solutions-1k-to-2k/solutions-1300-to-1399/solutions-1390-to-1399/solutions-1399/b/cpp/solution-1399-b.cpp
+++ problem-solutions-1k-to-2k/solutions-1300-to-1399/solutions-1390-to-1399/solutions-1399/b/cpp/solution-1399-b.cpp
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 int main()
 {
@@ -8,19 +10,33 @@ int main()
     while (t--)
     {
         int n;
-        std::cin << n;
+        std::cin >> n;
 
-        std::vector<int> a(n);
+        std::vector<long long> a(n);
         for (auto &it : a)
         {
             std::cin >> it;
         }
 
-        std::vector<int> b(n);
+        std::vector<long long> b(n);
         for (auto &it : b)
         {
             std::cin >> it;
         }
+
+        auto minA = *std::min_element(a.begin(), a.end());
+        auto minB = *std::min_element(b.begin(), b.end());
+
+        long long moves = 0;
+        
+        for (size_t index = 0; index < n; ++index)
+        {
+            long long movesA = a[index] - minA;
+            long long movesB = b[index] - minB;
+            moves += std::max(movesA, movesB);
+        }
+
+        std::cout << moves << std::endl;
     }
 
     return EXIT_SUCCESS;
