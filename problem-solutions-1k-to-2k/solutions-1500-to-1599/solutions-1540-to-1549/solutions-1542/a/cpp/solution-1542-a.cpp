b94ff76700e61028b2a284e80025d748c936f90e
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-07-03 16:35:14 +0200

    Add cpp solution to problem 1542 A.

---
+++ problem-solutions-1k-to-2k/solutions-1500-to-1599/solutions-1540-to-1549/solutions-1542/a/cpp/solution-1542-a.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main()
+{
+    int t;
+    std::cin >> t;
+
+    while (t--)
+    {
+        int n;
+        std::cin >> n;
+        int n2 = 2 * n;
+        int even = 0;
+        while (n2--)
+        {
+            int i;
+            std::cin >> i;
+            if (0 == i % 2)
+            {
+                even++;
+            }
+        }
+
+        // If even equals n then there must also be n odd numbers
+        // and these can be paired up to have a solution.
+        if (even == n)
+        {
+            std::cout << "Yes" << std::endl;
+        }
+        else
+        {
+            std::cout << "No" << std::endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
