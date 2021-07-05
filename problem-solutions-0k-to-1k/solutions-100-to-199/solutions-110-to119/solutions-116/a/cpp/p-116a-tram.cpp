274b0f11d0d46630db0af11e55af70e507cbb4f8
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2021-07-05 23:01:29 +0200

    Rework and move the CPP solution for problem 116 a.

--- problem-solutions-0k-to-1k/solutions-100-to-199/solutions-110-to119/solutions-116/a/cpp/p-116a-tram.cpp
+++ problem-solutions-0k-to-1k/solutions-100-to-199/solutions-110-to119/solutions-116/a/cpp/p-116a-tram.cpp
@@ -4,21 +4,27 @@ int main()
 {
     int numberOfStops;
     std::cin >> numberOfStops;
+
     int capacity = 0;
     int numberOfPassengers = 0;
-    for (int i = 0; i < numberOfStops; i++)
+    
+    for (size_t i = 0; i < numberOfStops; i++)
     {
         int leaving;
         std::cin >> leaving;
+        numberOfPassengers -= leaving;
+        
         int entering;
         std::cin >> entering;
         numberOfPassengers += entering;
-        numberOfPassengers -= leaving;
+        
         if (capacity < numberOfPassengers)
         {
             capacity = numberOfPassengers;
         }
     }
+
     std::cout << capacity;
-    return 0;
+    
+    return EXIT_SUCCESS;
 }
